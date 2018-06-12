d1bcc108df7b5022fc7013d2b00b62a1bc1bb340
grigosha40 <malax-ov@yandex.ru>
2018-06-12 11:03:12 +0300

    12 06 18

--- contact-form/src/Button.js
+++ contact-form/src/Button.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import Contact from './Contact';
 
 class Button_send extends Component {
     render() {
       return (
         
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" value="Submit" onClick={Contact.TextField.Send_function()}>
       Send
     </Button>
       )
@@ -15,5 +16,7 @@ class Button_send extends Component {
   
 
 };
-
+function Send_function() {
+  document.getElementById("form").submit();
+}
 export default Button_send;
--- contact-form/src/Contact.js
+++ contact-form/src/Contact.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
+import Button_send from './Button.js';
 class Contact extends Component {
     render() {
       return (
         
-         <TextField variant="Input" color="placeholder" >
+         <TextField variant="Input" color="placeholder" onClick={Contact.Send_function()} >
          Your message
         </TextField> 
        
       );
     }
   }
-  
+  function Send_function() {
+    document.getElementById("contact").submit();
+  }
   export default Contact;
