c483f6dba261ae61bfba3e0dabd7f605bf1ea38e
grigosha40 <malax-ov@yandex.ru>
2018-06-12 11:34:03 +0300

    12 06 2018 1133

--- contact-form/src/App.js
+++ contact-form/src/App.js
@@ -3,11 +3,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div></div>
-    );
+class App extends React.Component{
+  contactUs() {
+    $.ajax({
+      url: '/contactus',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+          // Success..
+      }.bind(this),
+      error: function(xhr, status, err) {
+          console.error(status, err.toString());
+      }.bind(this)
+  });
+
   }
-}
+  render(){
+    return (
+      <div>
+    <Input type="email" ref="contact_email" placeholder="Your email address"/>
+    <Input type="text" ref="contact_subject" placeholder="Subject"/>
+    <Input type="textarea" ref="contact_content" placeholder="Content"/>
+    <Button onClick={this.contactUs.bind(this)} bsStyle="primary" bsSize="large">Submit</Button>
+  </div>
+)
+  }
+};
+
 export default App;
